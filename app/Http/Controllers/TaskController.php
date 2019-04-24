f385ac3f11330329a1abba107984341b3d4c74b7
DimaSorokin <demonsorokin29@gmail.com>
2019-04-24 09:23:40 +0300

    Initial commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Task::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title'=>'required|string',
+            'completed' => 'required|boolean'
+        ]);
+        $task = Task::create($data);
+        return response($task, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Task::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        $data = $request->validate([
+            'title'=>'required|string',
+            'completed' => 'required|boolean'
+        ]);
+        $task->update($data);
+        return response($task, 201);
+    }
+    public function updateAll(Request $request){
+        $data = $request->validate([
+            'completed' => 'required|boolean'
+        ]);
+        Task::query()->update($data);
+        return response('Update', 200);
+    }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response('Deleted task item', 200);
+    }
+    public function destroyCompleted(Request $request)
+    {
+        $data = $request->validate([
+            'tasks' => 'required|array'
+        ]);
+        Task::destroy($request->todos);
+        return response('Delete', 200);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/tasks', 'TaskController@index');
+Route::post('/tasks', 'TaskController@store');
+Route::patch('/tasks/{task}', 'TaskController@update');
+Route::patch('/tasksCheckAll', 'TaskController@updateAll');
+Route::delete('/tasks/{task}', 'TaskController@destroy');
+Route::delete('/tasksDeleteCompleted', 'TaskController@destroyCompleted');
+
+
+
